67b87cddf1930247f35c17c2aa9caea5c834a209
foo <foo@mafoo.org.uk>
2012-11-21 22:06:24 +0000

    end of OK03 - untested

--- asm/main.s
+++ asm/main.s
@@ -1,16 +1,38 @@
 .section .init
 .globl _start
 _start:
+b main
+
+.section .text
+main:
+mov sp,#0x8000
+
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
 
 
-ldr r0,=0x20200000
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
+myval .req r4
+mov myval,#0
 
-mov r1,#1
-lsl r1,#16
-str r1,[r0,#40]
+loop$:
+eor myval,#1 /* invert myval */
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,myval
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
 
-loop$: 
+mov r2,#0x3F0000
+wait1$:
+sub r2,#1
+cmp r2,#0
+bne wait1$
+
 b loop$
